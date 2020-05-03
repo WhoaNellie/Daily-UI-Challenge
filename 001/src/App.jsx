e7a4d9b0c4405db70fa618e4bef0488b71a7c40b
Nellie <fabybacenelson@gmail.com>
2020-05-02 23:10:31 -0700

    logo and fonts

--- 001/src/App.jsx
+++ 001/src/App.jsx
@@ -4,11 +4,14 @@ function App(){
  return(
      <React.Fragment>
          <header>
-             <h1>Mushnik <span>&amp; Son</span></h1>
+             <h1>Mushnik</h1>
              <h2>Home of the World's most fabulous plant, Audrey II!</h2>
          </header>
          <main>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Little_Shop_of_Horrors_at_Grafton_High_School_07.jpg"/>
+             <div className="audrey2">
+             <img src="https://upload.wikimedia.org/wikipedia/commons/2/2d/Little_Shop_of_Horrors_at_Grafton_High_School_07.jpg"/>
+             </div>
+            
             <p>Found during a total eclipse of the sun on sale for $1.95, Audrey II is now the most famous plan in the world. Until now, our propritary growing method has been kept a secret, but now you too may grow your own Audrey 3, 4, 5... as many as you please!</p>
 
             <h3>Sign up to recieve a free clipping of the world's largest Flytrap, Audrey II!</h3>
